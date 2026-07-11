838cf1876d6ae3c3e9b22ad51a2d9a02211407b9
Hasan Alhawaj <hasan.alhawaj.bh@gmail.com>
2026-07-11 18:39:05 +0300

    first commit

---
+++ README.md
@@ -0,0 +1,5 @@
+# Mantine Vue Vite template
+
+Get started with the template by clicking `Use this template` button on the top of the page.
+
+[Documentation](https://mantine-vue.dev/guides/vite/)
---
+++ src/App.vue
@@ -0,0 +1,9 @@
+<script setup lang="ts">
+import "@mantine-vue/core/styles.css";
+import { MantineProvider } from "@mantine-vue/core";
+import { theme } from "./theme";
+</script>
+
+<template>
+  <MantineProvider :theme="theme">App</MantineProvider>
+</template>
